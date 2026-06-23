506e69e33d56be9b5a3b8559ae7b7bb0a30b378a
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-23 03:36:52 -0300

    total bill calculated

---
+++ bill-splitter.py
@@ -0,0 +1,11 @@
+running_total = 0
+
+num_of_friends = 4
+
+appetizers = 37.89
+main_courses = 57.34
+desserts = 39.39
+drinks = 64.21
+
+running_total += appetizers + main_courses + desserts + drinks
+print(f"Total bill so far: {running_total}")
